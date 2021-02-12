063885aee52162f0539cc14d097730d156d9e113
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2021-02-12 06:36:15 -0300

    criando estrutura do projeto

---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Menu.css'
+
+export default function Menu() {
+    return (
+        <aside className="Menu">
+            Componente Menu
+        </aside>
+    )
+}
